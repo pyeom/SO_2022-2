d256be2a540d58ed7dc4adb73d0092e91a32fb71
ScaryShiroyasha <diego.acevedos@usm.cl>
2022-09-08 00:16:04 -0300

    Update orden.c
    deberia funcionar

--- Laboratorio_1/orden.c
+++ Laboratorio_1/orden.c
@@ -4,67 +4,125 @@
 #include <dirent.h>
 
 
-void mover(){
-    DIR *dp = opendir("/test");
-    FILE *fp;
-    char *comando;
+void leer(char *nombre, char *directorio){
+    char buffer[50];
+    strcpy(buffer, directorio);
+    strcat(buffer, "/");
+    strcat(buffer, nombre);
+    FILE *fp = fopen(buffer, "r");
+    //printf ("el nombre es '%s'\n", buffer);
     int prioridad, anno;
-    struct dirent *de = NULL;
 
-    while ((de = readdir(dp))){ //Itera sobre cada File del directorio
-        de -> d_name;
+    if (fp == NULL)
+    {
+        printf("Error: could not open file %s \n", buffer);
+        return;
+    }
 
-        if((fp = fopen(d_name, "r")) != NULL){  //Abre el File y guarda los datos que contiene en anno y prioridad
-            fscanf(fp, "%d\n%d", &anno, &prioridad);
-        }
+    // read one character at a time and
+    // display it to the output
+    fscanf(fp, "%d\n%d", &anno, &prioridad);
+    char ch;
+    while ((ch = fgetc(fp)) != EOF)
+        putchar(ch);
 
-        fclose(fp);
-        
-        if ((*int)anno == 2019){
-
+    // close the file
+    //printf ("el anno es '%d' y su prioridad '%d' \n", anno, prioridad);
+    char aux[100];
+    if (anno == 2019){
              if (prioridad >= 9000){ //Mueve el file al folder correspondiente
-            system("mv %s ~/2019/9000+", d_name);
+                strcat(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2019/9000+");
+                system(aux);
             }
 
             else if (prioridad >= 6000){
-                system("mv %s ~/2019/6000+", d_name);
+                strcat(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2019/6000+");
+                system(aux);
             }
 
             else{
-                system("mv %s ~/2019/3000+", d_name);
+                strcpy(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2019/3000+");
+                system(aux);
             }
         }
-
-        else if ((*int)anno == 2020){
+        else if (anno == 2020){
             if (prioridad >= 9000){ //Mueve el file al folder correspondiente
-                system("mv %s ~/2020/9000+", d_name);
+                strcpy(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2020/9000+");
+                system(aux);
             }
 
             else if (prioridad >= 6000){
-                system("mv %s ~/2020/6000+", d_name);
+                strcpy(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2020/6000+");
+                system(aux);
             }
 
             else{
-                system("mv %s ~/2020/3000+", d_name);
+                strcpy(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2020/3000+");
+                system(aux);
             }
         }
 
         else{
             if (prioridad >= 9000){ //Mueve el file al folder correspondiente
-                system("mv %s ~/2021/9000+", d_name);
+                strcpy(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2021/9000+");
+                system(aux);
             }
 
             else if (prioridad >= 6000){
-                system("mv %s ~/2021/6000+", d_name);
+                strcpy(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2021/6000+");
+                system(aux);
             }
 
             else{
-                system("mv %s ~/2021/3000+", d_name);
+                strcpy(aux, "mv ");
+                strcat(aux, buffer);
+                strcat(aux, " ./2021/3000+");
+                system(aux);
             }
         }
-        
+    fclose(fp);
+}
 
+void mover () {
+    struct dirent *pDirent;
+    DIR *pDir;
+
+    // Ensure correct argument count.
+
+    // Ensure we can open directory.
+
+    pDir = opendir ("./test");
+    if (pDir == NULL) {
+        printf ("Cannot open directory '%s'\n", "test");
     }
-    closedir(dp);
 
+    // Process each entry.
+
+    while ((pDirent = readdir(pDir)) != NULL)  {
+        if((strcmp(pDirent->d_name,".")==0 || strcmp(pDirent->d_name,"..")==0 || (*pDirent->d_name) == '.' )){}
+        else {
+        printf ("[%s]\n", pDirent->d_name);
+        leer(pDirent->d_name, "./test");
+        }
+    }
+
+    // Close directory and exit.
+
+    closedir (pDir);
 }
